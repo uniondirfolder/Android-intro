6041653928aa3eedd38e78f6c7fc3fd478648803
NVV <uniondirfolder@gmail.com>
2021-05-20 15:46:38 +0300

    end step 2

--- app/nvv_2/src/main/java/com/example/nvv_2/MainActivity.java
+++ app/nvv_2/src/main/java/com/example/nvv_2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.nvv_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnClick(View view){
+        int id =view.getId();
+        switch (id){
+            case R.id.button:
+                Toast.makeText(this, "Button Clicked!",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
